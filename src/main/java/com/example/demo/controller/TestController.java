98eebbd1594b49d93b7ae3082e90ef8d0e9d7683
ShrawanDadarwal <shrawandadarwal@gmail.com>
2024-04-19 14:30:01 +0530

    upload data

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class TestController {
     public ResponseEntity<?> getTest() {
         // I am making mock service
         return ResponseEntity.ok("First custom App from Spring boot");
+    }
+    @PostMapping("/mock")
+    public ResponseEntity<?> postData(@RequestBody String content) {
+        // I am making mock service
+        return ResponseEntity.ok(content);
     }
 }
