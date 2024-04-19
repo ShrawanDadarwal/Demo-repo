ca4cc45a8d1c72c29e1cd88ee8cb990c8513146b
ShrawanDadarwal <shrawandadarwal@gmail.com>
2024-04-19 14:21:28 +0530

    alter code

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,15 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/democontroller")
 public class TestController {
 
-    @GetMapping
-    public ResponseEntity<?> getTest(){
+    @GetMapping("/")
+    public ResponseEntity<?> getTest() {
         // I am making mock service
         return ResponseEntity.ok("First custom App from Spring boot");
     }
